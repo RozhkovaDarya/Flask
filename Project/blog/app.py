2fd925d7cee0c22a184dd9c3d95b515721e05319
Daria <mironochka1@gmail.com>
2023-03-30 14:15:40 +0300

    lesson_2 Changed articles

--- Project/blog/app.py
+++ Project/blog/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask import request, g
 from werkzeug.exceptions import BadRequest
 from blog.views.users import users_app
+from blog.views.articles import articles_app
 
 app = Flask(__name__)
 
@@ -87,3 +88,4 @@ def process_after_request(response):
 
 
 app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
