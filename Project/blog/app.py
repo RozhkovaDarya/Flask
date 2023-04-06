48aa2d57e039041f08e67994bfccee18f0b41ef9
Daria <mironochka1@gmail.com>
2023-04-06 12:19:51 +0300

    lesson_3 Created commands

--- Project/blog/app.py
+++ Project/blog/app.py
@@ -88,6 +88,34 @@ def process_after_request(response):
     return response
 
 
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+
+    print("done! created users:", admin, james)
+
+
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
 
