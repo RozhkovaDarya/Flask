f8113ac5e0bd810599902191f08770c40ee3f959
Daria <mironochka1@gmail.com>
2023-04-10 12:29:57 +0300

    lesson_6 Added viev for creations articls

--- Project/blog/views/articles.py
+++ Project/blog/views/articles.py
@@ -22,3 +22,32 @@ def article_detals(article_id):
     if article is None:
         raise NotFound
     return render_template("articles/details.html", article=article)
+
+
+@articles_app.route("/create/", methods=["GET", "POST"], endpoint="create")
+@login_required
+def create_article():
+    error = None
+    form = CreateArticleForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        article = Article(title=form.title.data.strip(), body=form.body.data)
+        db.session.add(article)
+        if current_user.author:
+            # use existing author if present
+            article.author = current_user.author
+        else:
+            # otherwise create author record
+            author = Author(user_id=current_user.id)
+            db.session.add(author)
+            db.session.flush()
+            article.author = current_user.author
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create a new article!")
+            error = "Could not create article!"
+        else:
+            return redirect(url_for("articles_app.details", article_id=article.id))
+
+    return render_template("articles/create.html", form=form, error=error)
